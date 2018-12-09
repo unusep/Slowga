94dd16b87f28d2636c45c0cb155d1e281e3732e2
Deshun Cai <unusep@gmail.com>
2018-12-09 11:28:15 +0800

    WIP gesture control for manga detail view

--- Slowga/Pages/Manga Detail/MangaDetailView.swift
+++ Slowga/Pages/Manga Detail/MangaDetailView.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
+protocol MangaDetailViewDelegate {
+    func dismissVC()
+}
+
 class MangaDetailView: UIView {
     var mangaCover: MangaCover!
+    var delegate: MangaDetailViewDelegate?
     
     init(mangaCover: MangaCover){
         self.mangaCover = mangaCover
@@ -33,6 +38,17 @@ class MangaDetailView: UIView {
         imageView.layer.cornerRadius = 2
         imageView.clipsToBounds = true
         imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        button.addTarget(self, action: #selector(buttonWasTapped(sender:)), for: UIControl.Event.touchUpInside)
+        button.backgroundColor = .blue
+        self.addSubview(button)
+        
+    }
+    
+    @objc func buttonWasTapped(sender: UIButton) {
+        print("touched")
+        self.delegate?.dismissVC()
     }
     
     required init?(coder aDecoder: NSCoder) {
--- Slowga/Pages/Manga Detail/MangaDetailViewController.swift
+++ Slowga/Pages/Manga Detail/MangaDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MangaDetailViewController: UIViewController {
+class MangaDetailViewController: UIViewController, MangaDetailViewDelegate, UIGestureRecognizerDelegate {
     var mangaCover: MangaCover!
     
     override func viewDidLoad() {
@@ -17,27 +17,46 @@ class MangaDetailViewController: UIViewController {
         setupView()
     }
     
-    func setupView() {
+    @objc func backgroundViewWasTapped(_ sender: UITapGestureRecognizer? = nil) {
+        print("bg was tapped")
+        dismissVC()
+    }
+    
+    @objc func panGestureAction(_ panGesture: UIPanGestureRecognizer) {
+        print("attempted to pan or something")
+    }
+    
+    private func setupView() {
+        // set up background
         self.view.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
         
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundViewWasTapped(_:)))
+        tapGestureRecognizer.delegate = self
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+        self.view.isUserInteractionEnabled = true
+        
+        // set up detail view
         let detailView = MangaDetailView(mangaCover: mangaCover)
         self.view.addSubview(detailView)
-        
-        // layout detail view
         let safeArea = self.view.safeAreaLayoutGuide
         let padding: CGFloat = 10
+        let paddingTop: CGFloat = 20
         let cornerRadius: CGFloat = 10
         detailView.translatesAutoresizingMaskIntoConstraints = false
         detailView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: padding).isActive = true
         detailView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -padding).isActive = true
-        detailView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: padding).isActive = true
+        detailView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: paddingTop).isActive = true
         detailView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
 
         detailView.layer.cornerRadius = cornerRadius
         detailView.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
         detailView.clipsToBounds = true
         
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(_:)))
+        detailView.addGestureRecognizer(panGestureRecognizer)
         detailView.backgroundColor = PRIMARY_COLOR
+        
+        detailView.delegate = self
     }
     
     init(for mangaCover: MangaCover) {
@@ -48,5 +67,8 @@ class MangaDetailViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
+    
+    func dismissVC() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
